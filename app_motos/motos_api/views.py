a2a3a3146d810cdb799a68502c68d4c2c3d01e4b
LpzSteven <stevenlopez007r@gmail.com>
2023-07-02 12:02:05 -0500

    second commit

--- app_motos/motos_api/views.py
+++ app_motos/motos_api/views.py
@@ -30,5 +30,60 @@ class MotoListApiView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+class MotoDetailApiView(APIView):
+    def get_object(self, moto_id):
+        try:
+            return Moto.objects.get(id=moto_id)
+        except Moto.DoesNotExist:
+            return None
+    
+    def get(self, request, moto_id, *args, **kwargs):
+        moto_instance = self.get_object(moto_id)
+        if not moto_instance:
+            return Response(
+                {"res": "object with moto id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = MotoSerializer(moto_instance)
+        return Response(serializer.data, status= status.HTTP_200_OK)
+    
+    def put (self, request, moto_id, *args, **kwargs):
+        moto_instance= self.get_object(moto_id)
+        if not moto_instance:
+            return Response(
+                {"res": "Object with moto id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        data ={
+            'trademark': request.data.get('trademark'),
+            'model': request.data.get('model'),
+            'reference': request.data.get('reference'),
+            'price': request.data.get('price'),
+            'image': request.data.get('image'),
+            'supplier': request.data.get('supplier'),
+        }
+        serializer = MotoSerializer(
+            instance=moto_instance,
+            data=data,
+            partial= True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, moto_id, *args, **kwargs):
+        moto_instance = self.get_object(moto_id)
+        if not moto_instance:
+            return Response(
+                {"res":"Object with moto id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        moto_instance.delete()
+        return Response(
+            {"res":"Object delete!"},
+            status=status.HTTP_200_OK
+        )
+    
 
 
